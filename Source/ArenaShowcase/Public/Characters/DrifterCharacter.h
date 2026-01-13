be21dc684bf66a0684c15e00538598a2f6da09ac
ChronoForgeD <sony3554@gmail.com>
2026-01-12 20:21:48 -0500

    Change IDE from Visual Studio to Jet Brain Riders

---
+++ Source/ArenaShowcase/Public/Characters/DrifterCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/DrifterCharacterBase.h"
+#include "DrifterCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENASHOWCASE_API ADrifterCharacter : public ADrifterCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ArenaShowcase/Public/Characters/DrifterEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/DrifterCharacterBase.h"
+#include "DrifterEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENASHOWCASE_API ADrifterEnemy : public ADrifterCharacterBase
+{
+	GENERATED_BODY()
+	
+};
